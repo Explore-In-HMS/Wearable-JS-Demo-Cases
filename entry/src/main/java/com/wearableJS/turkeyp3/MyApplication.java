378d679ba075ab4e29d33ef5641b3068d394ce4e
Mehmet Yozgatli <mehmet.yozgatli1@huawei.com>
2021-09-20 09:46:21 +0300

    Initial Commit

---
+++ entry/src/main/java/com/wearableJS/turkeyp3/MyApplication.java
@@ -0,0 +1,10 @@
+package com.wearableJS.turkeyp3;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
