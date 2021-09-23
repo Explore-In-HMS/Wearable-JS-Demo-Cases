7b70ee94f854e664a8ff9ca1b0b192c46c9d6e69
z00612636 <zehra.yilmaz1@huawei.com>
2021-09-23 10:27:00 -0700

    add compass

--- entry/src/main/js/default/pages/compass/compass.js
+++ entry/src/main/js/default/pages/compass/compass.js
@@ -1,5 +1,35 @@
+import sensor from '@system.sensor';
+import app from '@system.app';
+
 export default {
     data: {
-        title: 'World'
+        compass_transform: "rotate(0deg)",
+    },
+    onInit() {
+        var _this = this;
+        _this.loadCompass();
+    },
+    onDestroy() {
+        sensor.unsubscribeCompass();
+    },
+    onBackPressed() {
+        sensor.unsubscribeCompass();
+    },
+    loadCompass() {
+        var _this = this;
+        sensor.subscribeCompass({
+            success: function (ret) {
+                //direction value is float
+                var direction = ret.direction;
+                //to get compass direction, extract from 360
+                var compassDirection = (360 - direction);
+                //rotate compass image
+                _this.compass_transform = "rotate(" + compassDirection + "deg)";
+                console.log("compass: compassDirection: " + direction);
+            },
+            fail: function (data, code) {
+                console.error('compass: subscribe compass fail, code: ' + code + ', data: ' + data);
+            },
+        });
     }
 }
