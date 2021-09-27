47f8febf91c257c8c5026eecc866fb46e164cedd
z00612636 <zehra.yilmaz1@huawei.com>
2021-09-27 00:14:10 -0700

    add accelerometer sensor

--- entry/src/main/js/default/pages/accelerometer/accelerometer.js
+++ entry/src/main/js/default/pages/accelerometer/accelerometer.js
@@ -1,5 +1,47 @@
+import sensor from '@system.sensor';
+import router from '@system.router';
+
 export default {
     data: {
-        title: 'World'
+        X: "--",
+        Y: "--",
+        Z: "--"
+    },
+    onInit() {
+        var _this = this;
+        _this.startAccelerometer();
+    },
+    startAccelerometer() {
+        var _this = this;
+        //1. game: Extremely high frequency (20 ms per callback), which is applicable to gaming.
+        //2. ui: High frequency (60 ms per callback), which is applicable to UI updating.
+        //3. normal: Regular frequency (200 ms per callback), which is application to low power consumption. The default value is normal.
+
+        sensor.subscribeAccelerometer({
+            interval: 'normal',
+            success: function (ret) {
+                console.log('get data x:' + ret.x);
+                console.log('get data y:' + ret.y);
+                console.log('get data z:' + ret.z);
+                _this.X = ret.x;
+                _this.Y = ret.y;
+                _this.Z = ret.z;
+            },
+            fail: function (data, code) {
+                console.error('subscribe accelerometer fail, code: ' + code + ', data: ' + data);
+            },
+        });
+    },
+    onDestroy() {
+        sensor.unsubscribeAccelerometer()
+    },
+    touchMove(e) { // Handle the swipe event.
+
+        if (e.direction == "right") // Swipe right to exit.
+        {
+            router.replace({
+                uri: 'pages/sensors/sensors'
+            })
+        }
     }
 }
