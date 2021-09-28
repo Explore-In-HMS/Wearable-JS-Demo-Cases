85551e981429549de9f01485240235628214fc99
Mehmet Yozgatli <mehmet.yozgatli1@huawei.com>
2021-09-28 11:09:12 +0300

    Developed Step Counter Sensor

--- entry/src/main/js/default/pages/stepcounter/stepcounter.js
+++ entry/src/main/js/default/pages/stepcounter/stepcounter.js
@@ -1,5 +1,47 @@
+import sensor from '@system.sensor';
+import router from '@system.router'
+
 export default {
     data: {
-        title: 'World'
+        stepCount: '--',
+        distance: '--',
+    },
+    onInit(){
+        this.getStepCount();
+    },
+    getStepCount(){
+        let _this = this;
+        sensor.subscribeStepCounter({
+            success: function (response){
+                console.log('get step value:' + response.steps);
+                _this.stepCount = response.steps + ' STEPS';
+                _this.calculateDistance(response.steps);
+            },
+            fail: function (data, code) {
+                console.log('subscribe step count fail, code:' + code + ', data:' + data);
+            },
+        });
+    },
+    calculateDistance(steps) {
+        const multiplyVal = 0.762;
+        let stepToMeter = parseInt(steps * multiplyVal);
+        if (stepToMeter < 1000) {
+            this.distance = stepToMeter + " metres"
+        } else {
+            this.distance = (stepToMeter / 1000).toFixed(1) + " km"
+        }
+    },
+    touchMove(e){
+        if (e.direction == "right") {
+            router.replace({
+                uri: 'pages/sensors/sensors'
+            });
+        }
+    },
+    stepCounterUnsubscribe(){
+        sensor.unsubscribeStepCounter();
+    },
+    onDestroy(){
+        this.barometerUnsubscribe();
     }
 }
