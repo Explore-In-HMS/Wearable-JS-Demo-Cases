7068b01cc3cf54ae9dd2148899e1dc65e88d8aa4
Ece Aydin 00598997 <ece.aydin@huawei.com>
2021-09-29 12:05:52 +0000

    Sensor - light & heart rate & body state

--- entry/src/main/js/default/pages/heartrate/heartrate.js
+++ entry/src/main/js/default/pages/heartrate/heartrate.js
@@ -1,5 +1,49 @@
+// @ts-nocheck
+import sensor from '@system.sensor';
+import router from '@system.router';
+
 export default {
     data: {
-        title: 'World'
+        heartRate: '--',
+        bodyStateValue: '--',
+    },
+    onInit() {
+        this.getHeartRate();
+        var _this = this;
+        sensor.subscribeOnBodyState({
+            success: function (ret) {
+                console.log('get on-body state value:' + ret.bodyStateValue);
+                if (ret.bodyStateValue === true) {
+                    _this.getHeartBeatRate();
+                }
+            },
+            fail: function (data, code) {
+                console.log('fail to get on body state, code:' + code + ', data: ' + data);
+            },
+        });
+    },
+    getHeartRate() {
+        var _this = this;
+        sensor.subscribeHeartRate({
+            success: function (ret) {
+                console.log('get heartRate value:' + ret.heartRate);
+                _this.heartRate = "HeartRate: " + ret.heartRate + " bpm";
+            },
+            fail: function (data, code) {
+                console.log('subscribe heart rate fail, code: ' + code + ', data: ' + data);
+                _this.heartRate = "HeartRate failed";
+            },
+        });
+    },
+    touchMove(e) {
+        if (e.direction == "right") {
+            router.replace({
+                uri: 'pages/sensors/sensors'
+            })
+        }
+    },
+    onDestroy() {
+        this.unsubscribeHeartRate();
+        this.unsubscribeOnBodyState();
     }
 }
