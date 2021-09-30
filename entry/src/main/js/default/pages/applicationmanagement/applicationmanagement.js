b58acc17feac5c61d64192f36be46af17089ec9a
z00612636 <zehra.yilmaz1@huawei.com>
2021-09-30 06:30:12 -0700

    add application management

--- entry/src/main/js/default/pages/applicationmanagement/applicationmanagement.js
+++ entry/src/main/js/default/pages/applicationmanagement/applicationmanagement.js
@@ -1,5 +1,50 @@
+import router from '@system.router';
+import prompt from '@system.prompt'
+import pkg from '@system.package';
 export default {
     data: {
-        title: 'World'
+        packageNameText : "--",
+        installed : "--"
+    },
+    onTextChange(e){
+        prompt.showToast({
+            message: "value: " + e.value,
+            duration: 3000,
+        });
+        this.packageNameText = e.value;
+    },
+    buttonClick(e){
+        this.$element("input").showError({
+            error: 'error text'
+        });
+        if (this.packageNameText != null) {
+            checkIfAppInstalled(this.packageNameText)
+        }
+    },
+    checkIfAppInstalled(packageName){
+        var installed ;
+        pkg.hasInstalled({
+            bundleName: packageName,
+            success: function(data) {
+                installed = "Yes"
+                console.log('package has installed: ' + data);
+            },
+            fail: function(data, code) {
+                installed = "No"
+                console.log('query package fail, code: ' + code + ', data: ' + data);
+            },
+        });
+        return installed;
+    },
+
+    touchMove(e) { // Handle the swipe event.
+
+        if (e.direction == "right") // Swipe right to exit.
+        {
+            router.replace({
+                uri: 'pages/systemcapabilities/systemcapabilities'
+            })
+        }
     }
+
 }
