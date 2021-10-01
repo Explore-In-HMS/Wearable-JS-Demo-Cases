7d29f730b4368b480bbaa03dfb9a99cf79b4f114
Mehmet Yozgatli <mehmet.yozgatli1@huawei.com>
2021-10-01 17:31:17 +0300

    Developed Geographic Location Function

---
+++ entry/src/main/js/default/pages/geographicLocationSubscribe/geographicLocationSubscribe.js
@@ -0,0 +1,38 @@
+import router from '@system.router'
+import geolocation from '@system.geolocation';
+
+export default {
+    data: {
+        longitude: '--',
+        latitude: '--',
+        altitude: '--',
+        accuracy: '--',
+        time: '--'
+    },
+    onInit(){
+        var _this = this;
+        geolocation.subscribe({
+            success: function(data) {
+                console.log('get location. latitude:' + data.latitude);
+                _this.longitude = data.longitude;
+                _this.latitude = data.latitude;
+                _this.altitude = data.altitude;
+                _this.accuracy = data.accuracy;
+                _this.time = data.time;
+            },
+            fail: function(data, code) {
+                console.log('fail to get location. code:' + code + ', data:' + data);
+            },
+        });
+    },
+    touchMove(e) {
+        if (e.direction == "right") {
+            router.replace({
+                uri: 'pages/geographiclocation/geographiclocation'
+            });
+        }
+    },
+    onDestroy(){
+        geolocation.unsubscribe();
+    }
+}
--- entry/src/main/js/default/pages/geographiclocation/geographiclocation.js
+++ entry/src/main/js/default/pages/geographiclocation/geographiclocation.js
@@ -1,5 +1,41 @@
+import router from '@system.router'
+import geolocation from '@system.geolocation';
+
 export default {
     data: {
-        title: 'World'
+        locationType: '--',
+        coordType: '--'
+    },
+    onInit(){
+        var _this = this;
+        geolocation.getLocationType({
+            success: function(data) {
+                console.log('success get location type:' + data.types[0]);
+                _this.locationType = data.types[0];
+            },
+            fail: function(data, code) {
+                console.log('fail to get location. code:' + code + ', data:' + data);
+            },
+        });
+
+        var types = geolocation.getSupportedCoordTypes();
+        _this.coordType = types;
+    },
+    showLocationInfo(){
+        router.push({
+            uri: 'pages/geographicLocationInfo/geographicLocationInfo'
+        })
+    },
+    showLocationSubscribe(){
+        router.push({
+            uri: 'pages/geographicLocationSubscribe/geographicLocationSubscribe'
+        })
+    },
+    touchMove(e){
+    if (e.direction == "right") {
+        router.replace({
+            uri: 'pages/systemcapabilities/systemcapabilities'
+        });
     }
+}
 }
