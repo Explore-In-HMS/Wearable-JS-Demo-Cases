79eda3d3ed9abb68a36c118b5aefe80d7015ef58
z00612636 <zehra.yilmaz1@huawei.com>
2021-10-01 02:08:27 -0700

    add text

--- entry/src/main/js/default/pages/brightness/brightness.js
+++ entry/src/main/js/default/pages/brightness/brightness.js
@@ -1,5 +1,21 @@
+import brightness from '@system.brightness';
 export default {
     data: {
-        title: 'World'
+        brightnessValue: '--'
+    },
+    onInit(){
+        this.getBrightness()
+    },
+    getBrightness(){
+        var _this = this
+        brightness.getValue({
+            success: function(data){
+                _this.brightnessValue = data.value
+                console.log('success get brightness value:' + data.value);
+            },
+            fail: function(data, code) {
+                console.log('get brightness fail, code: ' + code + ', data: ' + data);
+            },
+        });
     }
 }
