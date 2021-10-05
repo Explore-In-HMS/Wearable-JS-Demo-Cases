9ca53c5009b61187df690e5d60ad44d75e13d28d
z00612636 <zehra.yilmaz1@huawei.com>
2021-10-05 08:41:44 -0700

    remove input fields

--- entry/src/main/js/default/pages/brightness/brightness.js
+++ entry/src/main/js/default/pages/brightness/brightness.js
@@ -1,46 +1,38 @@
 import router from '@system.router';
 import prompt from '@system.prompt';
 import brightness from '@system.brightness';
+
 export default {
     data: {
         brightnessValue: '--'
     },
-    onInit(){
+    onInit() {
         this.getBrightness()
     },
-    getBrightness(){
+    getBrightness() {
         var _this = this
         brightness.getValue({
-            success: function(data){
+            success: function (data) {
                 _this.brightnessValue = data.value
                 console.log('success get brightness value:' + data.value);
             },
-            fail: function(data, code) {
+            fail: function (data, code) {
                 console.log('get brightness fail, code: ' + code + ', data: ' + data);
             },
         });
     },
-    setBrightness(value){
-        brightness.setValue(value)
+    buttonClickTwenty(e) {
+        brightness.setValue(20)
     },
-    onTextChange(e){
-        prompt.showToast({
-            message: "value: " + e.value,
-            duration: 3000,
-        });
-        this.setBrightness(e.value)
+    buttonClickFifty(e) {
+        brightness.setValue(50)
     },
-    buttonClick(e){
-        this.$element("input").showError({
-            error: 'error text'
-        });
-        if (this.packageNameText != null) {
-            this.checkIfAppInstalled(this.packageNameText)
-        }
+    buttonClickEighty(e) {
+        brightness.setValue(80)
     },
-    touchMove(e) {  // Handle the swipe event.
+    touchMove(e) { // Handle the swipe event.
 
-        if(e.direction == "right")  // Swipe right to exit.
+        if (e.direction == "right") // Swipe right to exit.
         {
             router.replace({
                 uri: 'pages/systemcapabilities/systemcapabilities'
