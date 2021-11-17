9cccd15b0e2864ab0ab3f339f1ca5a298b94af09
e00598997 <ece.aydin@huawei.com>
2021-11-17 20:29:21 +0300

    fix

--- entry/src/main/js/default/pages/distributedStart/distributedStart.js
+++ entry/src/main/js/default/pages/distributedStart/distributedStart.js
@@ -1,20 +1,56 @@
+// @ts-nocheck
+import router from '@system.router'
+import prompt from '@system.prompt';
+
 export default {
-    start() {
+    data: {
+        deviceList: ["Test1", "Test2"]
+    },
+    async doDistributeLocal() {
         let actionData = {
             uri: 'www.huawei.com'
         };
         let target = {
-            bundleName: "com.example.harmonydevsample",
-            abilityName: "com.example.harmonydevsample.EntryJSApiAbility",
+            bundleName: "com.myapps.harmonyappjava",
+            abilityName: "com.myapps.harmonyappjava.MainAbility",
+            data: actionData
+        };
+
+        let result = await FeatureAbility.startAbility(target);
+        if (result.code == 0) {
+            console.log('success');
+        } else {
+            console.log('cannot start browsing service, reason: ' + result.data);
+        }
+    },
+    async selectDevice(networkId) {
+        this.$element('showDialog').close();
+        let actionData = {
+            networkId: this.localNetWorkId,
+        };
+
+        let target = {
+            bundleName: "com.myapps.harmonyappjava",
+            abilityName: "com.myapps.harmonyappjava.MainAbility",
+            networkId: networkId,
             data: actionData
         };
 
         let result = await FeatureAbility.startAbility(target);
         let ret = JSON.parse(result);
         if (ret.code == 0) {
-            console.log('Browsing service started successfully.');
-        } else {
-            console.log('Failed to start the browsing service. Reason: ' + ret.data);
+            prompt.showToast({
+                message: 'OK',
+                duration: 1000,
+            });
+        }
+    },
+    touchMove(e) {
+
+        if (e.direction == "right") {
+            router.replace({
+                uri: 'pages/distributedCapability/distributedCapability'
+            })
         }
     }
 }
--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -2,34 +2,37 @@ import app from '@system.app';
 import router from '@system.router'
 
 export default {
-
-    touchMove(e){  // Handle the swipe event.
-        if(e.direction == "right") { // Swipe right to exit.
+    touchMove(e) { // Handle the swipe event.
+        if (e.direction == "right") { // Swipe right to exit.
             this.appExit();
         }
     },
-    startSystemCapabilities(){
-     router.push({
+    startSystemCapabilities() {
+        router.push({
             uri: 'pages/systemcapabilities/systemcapabilities'
         });
     },
-    startBasicFeatures(){
+    startBasicFeatures() {
         router.push({
             uri: 'pages/basicFeatures/basicFeatures'
         });
     },
-    startFileData(){
+    startFileData() {
         router.push({
             uri: 'pages/fileData/fileData'
         });
     },
-    startNetworkAccess(){
+    startNetworkAccess() {
         router.push({
             uri: 'pages/networkAccess/networkAccess'
         });
     },
-    appExit(){  // Exit the application.
+    startDistributedCapability() {
+        router.push({
+            uri: 'pages/distributedCapability/distributedCapability'
+        });
+    },
+    appExit() { // Exit the application.
         app.terminate();
     }
 }
-
